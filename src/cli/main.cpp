716b573d8a0906f027a992c06f35307bcb31c610
psyirius <psyirius@gmail.com>
2022-02-10 16:27:07 +0530

    Dynamic executable basename in cli usage.

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -4,9 +4,21 @@
 
 #include "jsxbin.h"
 
+string get_filename(const char* path) {
+    for(size_t i = strlen(path) - 1; i; --i)
+    {
+        if ((path[i] == '/') || (path[i] == '\\'))
+        {
+            return &path[i + 1];
+        }
+    }
+    return path;
+}
+
 int main(int argc, char* argv[]) {
-    if (argc != 3){
-        std::cout << "Usage: jsxbin_decompiler <jsxbin path> <output path>" << std::endl;
+    if (argc != 3) {
+        std::string cli_path = argv[0];
+        std::cout << "Usage: " << get_filename(cli_path.c_str()) << " <jsxbin path> <output path>" << std::endl;
     } else {
         std::string input_path = argv[1];
         std::string output_path = argv[2];
