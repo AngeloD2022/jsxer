baf03b4a4b23f6114edd34a856d4e3b4671f3897
angelodeluca <angelo.deluca.2022@kenstonapps.org>
2022-02-15 16:37:30 -0500

    Added MemberExpression.cpp again after unexplainably disappearing.

---
+++ src/jsxer/nodes/MemberExpression.cpp
@@ -0,0 +1,31 @@
+#include "MemberExpression.h"
+#include "../util.h"
+
+void MemberExpression::parse() {
+    memberInfo = decoders::d_ref(reader);
+    objInfo = decoders::d_node(reader);
+}
+
+string MemberExpression::to_string() {
+    string result = (objInfo == nullptr ? "" : objInfo->to_string());
+
+    if (decoders::is_integer(result) || (objInfo->type() == NodeType::BinaryExpression)) {
+        result = '(' + result + ')';
+    }
+
+    // Check member validity...
+    if (decoders::valid_id(memberInfo.id)) {
+        result += '.' + utils::to_string(memberInfo.id);
+    } else {
+        result += '[';
+        // check if ID can be converted to an integer...
+        if(decoders::is_integer(memberInfo.id)) {
+            result += utils::to_string(memberInfo.id);
+        } else {
+            result += utils::to_string_literal(memberInfo.id);
+        }
+        result += ']';
+    }
+
+    return result;
+}
