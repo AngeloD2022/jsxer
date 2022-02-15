f7e4cabd2fd0aff82344907086def5d4d0cdc883
angelodeluca <angelo.deluca.2022@kenstonapps.org>
2022-02-15 16:01:31 -0500

    Revert "Implemented part of simplify_number_literal"
    This reverts commit fc69c6f78302eb978f0ab411a617dacf09da0533.

--- src/jsxer/util.cpp
+++ src/jsxer/util.cpp
@@ -267,31 +267,7 @@ string simplify_number_literal(const string& value) {
     // - stripping off excess suffix zeroes for doubles.
     // - stripping off excess prefix zeroes for integers.
     // - formatting scientific number literal (eg: 1e5, 1.72e+5, etc).
-
-    // 0001232.42903000
-    string result = value;
-
-    if (result[0] == '0') {
-        for (int i = 0; i < result.length(); ++i) {
-            if (result[i] != '0') {
-                result = result.substr(i, result.length() - i);
-                break;
-            }
-        }
-    }
-
-    // check if string represents double
-    if (result.find('.') != string::npos && result[result.length() - 1] == '0') {
-        // only circumstance where 0 at the end is meaningless.
-        for (int i = (int)result.length()-1; i >= 0; --i) {
-            if (result[i] != '0'){
-                result = result.substr(0, i + 1);
-                break;
-            }
-        }
-    }
-
-    return result;
+    return value;
 }
 
 // TODO: fix formatting and rounding as in es
