70ff1a344721a9fbbe5dc45534ea3278da6d326b
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2022-11-29 22:05:49 -0500

    Removed unused code.

--- src/jsxer/deobfuscation.h
+++ src/jsxer/deobfuscation.h
@@ -8,16 +8,6 @@ using namespace std;
 BEGIN_NS(jsxer)
 BEGIN_NS(deob)
 
-//class Deobfuscation {
-//public:
-//    explicit Deobfuscation(vector<Number> *reserved_ids);
-//
-//    bool should_substitute(Number id, const ByteString& symbol);
-//
-//private:
-//    vector<Number> *_reserved_ids;
-//};
-
 /// Determines if renaming is appropriate with symbols in JSXBIN files that are obfuscated with Jsxblind...
 /// \param symbol the symbol name
 /// \return
