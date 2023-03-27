4112314d129bb54727cf5c068265f0b03bc1bdfd
psyirius <psyirius@gmail.com>
2023-03-27 18:49:45 +0530

    updated compiler dep pkg to latest commit

--- tools/test-runner.js/src/cli/index.ts
+++ tools/test-runner.js/src/cli/index.ts
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
+const fs = require('node:fs');
+const esc = require('es-compile');
+
 const args = process.argv;
+
+if (args.length >= 2) {
+    const input = args[0];
+    const output = args[1];
+
+    const contents = fs.readFileSync(input);
+
+    const result = esc.compileToJSXBIN(contents);
+
+    if (result.code === 0) {
+        fs.writeFileSync(output, result.output);
+    } else {
+        console.error(result.error);
+        process.exit(result.code);
+    }
+} else {
+    console.error('expected args: 2, supplied: ' + args.length);
+    process.exit(1);
+}
